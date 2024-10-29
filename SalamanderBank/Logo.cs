c007b86a980be7877c6659a23de5b61056942067
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-29 09:30:43 +0100

    Created methods to display Logos

--- SalamanderBank/Logo.cs
+++ SalamanderBank/Logo.cs
@@ -1,6 +1,66 @@
 ﻿namespace SalamanderBank;
 
-public class Logo
+public static class Logo
 {
-    
+    private const string FireLogo = @"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⠄⠀⠀⠀⠀⠀⠙⠛⢿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣿⣆⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⡿⠁⠀⠀⠀⠀⠀⢤⣀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣶⣤⣤⣤⣴⣾⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⢻⣷⣄⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⠟⠋⢉⣉⣉⣙⣿⣷⣤⣾⡀⠀⠀⠀⠀⣸⣿⣿⣦⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢠⡄⠀⣸⡿⠁⢀⣴⣿⣿⡿⢿⠿⣿⣿⣿⣷⣀⣀⣀⣴⣿⣿⣿⣿⡆⠀
+⠀⠀⠀⠀⠀⠀⠀⢸⣿⣷⣾⣿⡇⠀⠸⣿⣿⣿⡇⣀⠐⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀
+⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⡀⠀⠙⠿⣿⣿⣿⠀⢸⣿⣿⣿⣿⡉⠛⢻⣿⣿⣿⣿⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣷⣄⠀⠀⠈⠉⠁⠀⠘⠛⠿⠿⢉⣴⣤⣿⣿⣿⣿⡿⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠘⠿⠁⠈⠙⠛⢿⣿⠃⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⡟⢻⠟⠁⣠⣦⣤⡀⠀⣀⣀⣀⡀⠀⠀⠀⢀⣾⠏⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣧⡄⢠⣴⣾⣿⣿⣿⡄⠺⢿⣿⣿⣿⣶⣄⣴⡿⠃⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⠿⣿⣿⣿⣿⣯⣤⣀⣿⣿⣿⣿⡿⠟⠉⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠛⠛⠛⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀";
+
+    private const string TextLogo = @"
+███████  █████  ██       █████  ███    ███  █████  ███    ██ ██████  ███████ ██████  
+██      ██   ██ ██      ██   ██ ████  ████ ██   ██ ████   ██ ██   ██ ██      ██   ██ 
+███████ ███████ ██      ███████ ██ ████ ██ ███████ ██ ██  ██ ██   ██ █████   ██████  
+     ██ ██   ██ ██      ██   ██ ██  ██  ██ ██   ██ ██  ██ ██ ██   ██ ██      ██   ██ 
+███████ ██   ██ ███████ ██   ██ ██      ██ ██   ██ ██   ████ ██████  ███████ ██   ██ ";
+
+    public static void DisplayFullLogo()
+    {
+        int leftPadding = 67;
+        
+        var fireLines = FireLogo.Split(Environment.NewLine);
+        foreach (var line in fireLines)
+        {
+            Console.WriteLine(line.PadLeft(line.Length + leftPadding));
+        }
+        
+        var textLines = TextLogo.Split(Environment.NewLine);
+        foreach (var line in textLines)
+        {
+            int padding = (Console.WindowWidth - line.Length) / 2;
+            Console.WriteLine(line.PadLeft(line.Length + padding));
+        }
+    }
+
+    public static void DisplayFireLogo()
+    {
+        int leftPadding = 67;
+        
+        var fireLines = FireLogo.Split(Environment.NewLine);
+        foreach (var line in fireLines)
+        {
+            Console.WriteLine(line.PadLeft(line.Length + leftPadding));
+        }
+    }
+
+    public static void DisplayTextLogo()
+    {
+        var textLines = TextLogo.Split(Environment.NewLine);
+        foreach (var line in textLines)
+        {
+            int padding = (Console.WindowWidth - line.Length) / 2;
+            Console.WriteLine(line.PadLeft(line.Length + padding));
+        }
+    }
 }
