dd47dac02483a7f67899b0c5f6409d56dfef865b
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-29 21:24:03 +0100

    Changed test code

--- SalamanderBank/Program.cs
+++ SalamanderBank/Program.cs
@@ -2,10 +2,18 @@
 
 class Program
 {
+    // rasmuswenngren@hotmail.com
+    // matheus-torrico@outlook.com
+    // onni.bucht@me.com
+    // anton.dahlstrom@hotmail.com
     public static void Main(string[] args)
     {
-        Database db = new Database("salamanderbank.db");
-        db.InitializeDatabase();
-        db.AddUser(007, 1, "123abc", "james@bond.com", "James", "Bond");
+        // Database db = new Database("salamanderbank.db");
+        // db.InitializeDatabase();
+        // db.AddUser(007, 1, "123abc", "james@bond.com", "James", "Bond");
+        
+        // EmailService.SendEmail($"Hello Mauricio and welcome to Salamander Bank! To get started, verify your account using the code below. \n{Guid.NewGuid()}");
+        
+        EmailService.SendEmail("Mauricio", "Corte.Mauricio98@gmail.com", EmailService.VerificationText("Mauricio"));
     }
 }
