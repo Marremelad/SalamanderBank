92416902088cf1009f26282211055fcbea4f6096
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-30 11:42:58 +0100

    Removed more test code

--- SalamanderBank/Program.cs
+++ SalamanderBank/Program.cs
@@ -4,14 +4,8 @@ namespace SalamanderBank;
 
 class Program
 {
-    // rasmuswenngren@hotmail.com
-    // matheus-torrico@outlook.com
-    // onni.bucht@me.com
-    // anton.dahlstrom@hotmail.com
     public static void Main(string[] args)
     {
-        //EmailService.SendEmail("Mauricio", "mauricio.corte@chasacademy.se", "Verification",EmailService.VerificationText("Mauricio"));
-
-        // Logo.DisplayFullLogo();
+        
     }
 }
