5ce3b662a9c15c1e2829efa2cbda5e87be72ce16
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-30 11:42:20 +0100

    Removed test code

--- SalamanderBank/Program.cs
+++ SalamanderBank/Program.cs
@@ -10,12 +10,8 @@ class Program
     // anton.dahlstrom@hotmail.com
     public static void Main(string[] args)
     {
-        for (int i = 0; i < 3; i++)
-        {
-            EmailService.SendEmail("Anton", "anton.dahlstrom@hotmail.com", "Verification",EmailService.VerificationText("Anton"));
-            EmailService.SendEmail("Mauricio", "mauricio.corte@chasacademy.se", "Verification",EmailService.VerificationText("Mauricio"));
-            Thread.Sleep(5000);
-        }
+        //EmailService.SendEmail("Mauricio", "mauricio.corte@chasacademy.se", "Verification",EmailService.VerificationText("Mauricio"));
+
         // Logo.DisplayFullLogo();
     }
 }
