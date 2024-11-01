a3e907dcb5283eb0e8781396f97330f70ef055ed
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-11-01 10:32:56 +0100

    Created class for sending sms

--- SalamanderBank/SmsService.cs
+++ SalamanderBank/SmsService.cs
@@ -1,6 +1,30 @@
-﻿namespace SalamanderBank;
+﻿using System.Net.Http.Headers;
+using DotNetEnv;
+
+namespace SalamanderBank;
 
 public class SmsService
 {
-    
+    public static async Task SendSms()
+    {
+        Env.Load("./Credentials.env");
+        var smsApiUsername = Env.GetString("SMS_API_USERNAME");
+        var smsApiPassword = Env.GetString("SMS_API_PASSWORD");
+
+        using var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+            "Basic", Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes($"{smsApiUsername}:{smsApiPassword}")));
+
+        var data = new List<KeyValuePair<string, string>>
+        {
+            new KeyValuePair<string, string>("from", "Salamander"),
+            new KeyValuePair<string, string>("to", "+46739496358"),
+            new KeyValuePair<string, string>("message", "Hello from team Salamander")
+        };
+
+        using var content = new FormUrlEncodedContent(data);
+        using var response = await httpClient.PostAsync("https://api.46elks.com/a1/sms", content);
+        string responseContent = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(responseContent);
+    }
 }
