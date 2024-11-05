36b7547373113dd6a52b7663b93deb223736ea72
Anton <anton.dahlstrom@hotmail.com>
2024-11-05 12:36:31 +0100

    add currency class

---
+++ SalamanderBank/Currency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalamanderBank
+{
+    public class Currency
+    {
+        public int ID;
+        public string Currency_code;
+        public float Exchange_rate;
+    }
+}
