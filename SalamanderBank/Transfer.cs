218a1083a0327ba3b9f1b3c61b4e90c4b5654f19
Anton <anton.dahlstrom@hotmail.com>
2024-11-05 15:16:47 +0100

    add transfer class

---
+++ SalamanderBank/Transfer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalamanderBank
+{
+    internal class Transfer
+    {
+        public int ID;
+        public User Sender_user_id;
+        public Account Sender_account_id;
+        public User Reciever_user_id;
+        public Account Reciever_account_id;
+        public decimal Amount;
+        public int Processed;
+    }
+}
