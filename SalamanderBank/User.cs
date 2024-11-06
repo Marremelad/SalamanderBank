ef1d689c61471619dd204e31f01df373a258c0ed
onni82 <onni.bucht@me.com>
2024-11-06 19:10:34 +0100

    Updated SQL queries and User class properties

--- SalamanderBank/User.cs
+++ SalamanderBank/User.cs
@@ -5,8 +5,8 @@ namespace SalamanderBank
     {
         public int ID { get; set; }
         public int Type { get; set; }
-        public string First_name { get; set; }
-        public string Last_name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string Telephone { get; set; }
